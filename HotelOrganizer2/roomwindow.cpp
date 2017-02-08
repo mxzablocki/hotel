0bfe1c641b4eebbcbe81ca00467c3c1834c073c6
Pawel Kudzia <pawel.kudzia1@gmail.com>
2017-02-08 21:16:12 +0100

    Drobne poprawki z oknem zarzadzania pokojami

--- HotelOrganizer2/roomwindow.cpp
+++ HotelOrganizer2/roomwindow.cpp
@@ -6,11 +6,15 @@ RoomWindow::RoomWindow(QWidget *parent) :
     ui(new Ui::RoomWindow)
 {
     ui->setupUi(this);
+    adminWindow = nullptr;
 }
 
 RoomWindow::~RoomWindow()
 {
     delete ui;
+    // zwalnianie okna zarzadzania klientami
+    // musi byc zwolnione, poniewaz tworzone jest bez uzycia parametru (okno nie ma "rodzica")
+    delete adminWindow;
 }
 
 void RoomWindow::on_pushButtonManageRooms_clicked()
