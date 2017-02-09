8967992d7ff04a8ecbbcd3372c31485862641292
Pawel Kudzia <pawel.kudzia1@gmail.com>
2017-02-09 23:14:17 +0100

    Dodanie komentarzy to do do metod (okno pokoje)

--- HotelOrganizer2/roomwindow.cpp
+++ HotelOrganizer2/roomwindow.cpp
@@ -25,19 +25,23 @@ void RoomWindow::on_pushButtonManageRooms_clicked()
 void RoomWindow::on_pushButtonRoomAdd_clicked()
 {
 	QMessageBox::information(this, "Info", "klikn¹³eœ Dodaj");
+	// TODO: obsluzyc dodawanie
 }
 
 void RoomWindow::on_pushButtonRoomSearch_clicked()
 {
 	QMessageBox::information(this, "Info", "klikn¹³eœ Szukaj");
+	// TODO: obsluzyc szukanie
 }
 
 void RoomWindow::on_pushButtonRoomEdit_clicked()
 {
 	QMessageBox::information(this, "Info", "klikn¹³eœ Edytuj");
+	// TODO: obsluzyc edycje
 }
 
 void RoomWindow::on_pushButtonRoomDelete_clicked()
 {
 	QMessageBox::information(this, "Info", "klikn¹³eœ Usuñ");
+	// TODO: obsluzyc usuwanie
 }
