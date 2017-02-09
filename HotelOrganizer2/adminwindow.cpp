3a88dfb4fe347e097e121c671c0a5d2575fe60f3
Pawel Kudzia <pawel.kudzia1@gmail.com>
2017-02-09 23:20:08 +0100

    Dodanie zwalniania pamieci (okno zarzadzaj klientami)

--- HotelOrganizer2/adminwindow.cpp
+++ HotelOrganizer2/adminwindow.cpp
@@ -7,11 +7,15 @@ AdminWindow::AdminWindow(QWidget *parent) :
 	ui(new Ui::AdminWindow)
 {
 	ui->setupUi(this);
+	roomWindow = nullptr;
 }
 
 AdminWindow::~AdminWindow()
 {
 	delete ui;
+	// zwalnianie okna zarzadzania klientami
+	// musi byc zwolnione, poniewaz tworzone jest bez uzycia parametru (okno nie ma "rodzica")
+	delete roomWindow;
 }
 
 void AdminWindow::on_pushButtonManageRooms_clicked()
