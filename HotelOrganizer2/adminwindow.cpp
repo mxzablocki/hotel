6888efcc4e3da088b534a1b14a9dbab28598a0a4
jozuel <jozuel666@gmail.com>
2017-02-10 19:15:05 +0100

    dodawanie do bazy//trzeba przetestowac

--- HotelOrganizer2/adminwindow.cpp
+++ HotelOrganizer2/adminwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_adminwindow.h"
 #include <QMessageBox>
 #include <QString>
+#include "BazaDanych.h"
 
 AdminWindow::AdminWindow(QWidget *parent) :
 	QMainWindow(parent),
@@ -26,9 +27,44 @@ void AdminWindow::on_pushButtonManageRooms_clicked()
 	roomWindow = new RoomWindow();
 	roomWindow->show();
 }
-
+int AdminWindow::szukajDanychWPolu(string dana, string** tabela, int iloscDanych, int nrPola)
+	for (int i = 0; i < iloscDanych; i++)
+	{
+		if (klienci[i][nrPola] == dana)
+		{
+			return atoi(dana[i][0]);	//jak znajdzie to zwraca id
+		}
+	}
+	return -1;
+}
+void AdminWindow::konwertujStringNaDate(string StrData, struct tm *timeinfo)	//format daty yyyy-mm-dd
+{	//nie testowalem tego nie wiem czy dziala
+	time_t rawtime;
+	//struct tm * timeinfo;
+	int year, month, day;
+	year = atoi(StrData.substr(0, 4));//rok
+	month = atoi(StrData.substr(5, 2));//miesiac
+	month = atoi(StrData.substr(8, 2))//miesiac 
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	timeinfo->tm_year = year - 1900;
+	timeinfo->tm_mon = month - 1;
+	timeinfo->tm_mday = day;
+	mktime(timeinfo);
+}
 void AdminWindow::on_pushButtonClientAdd_clicked()
 {
+	string **klienci = nullptr;
+	string **pokoje = nullptr;
+	BazaDanych * baza;
+	string* rezerwacja, klient;
+	int iloscDanych;
+	int idKlienta = -1;
+	int idPokoju = -1;
+	bool czyIstnieje = false;
+	rezerwacja = new string[6];
+	baza = new BazaDanych();
+	iloscDanych = baza->select("klienci", klienci);
 	QMessageBox::information(this, "Info", "kliknąłeś Dodaj");
 	// TODO: obsluzyc dodawanie
 
@@ -40,11 +76,49 @@ void AdminWindow::on_pushButtonClientAdd_clicked()
 	QString clientDays = ui->spinBoxClientDays->text();
 	QString clientRoomNumber = ui->spinBoxClientRoomNumber->text();
 	QString clientPeople = ui->spinBoxClientPeople->text();
-
-	ui->listWidgetClients->addItem(
-		clientName + " " + clientSurname + " " + clientPesel + " " + 
-		clientDate + " " + clientDays + " " + clientRoomNumber + " " + clientPeople
-	);
+	idKlienta = szukajDanychWPolu(clientPesel.toStdString(), klienci, iloscDanych, 3);
+	if(idKlienta != -1)
+	{
+		rezerwacja[2] = idKlienta;
+	}
+	else	//jesli nie ma takiego klienta to go dodaje
+	{
+		klient = new string[5];
+		klient[1] = clientName.toStdString();
+		klient[2] = clientSurname.toStdString();
+		klient[3] = clientPesel.toStdString();
+		klient[4] = "0";		//telefon;
+		klient[5] = "0";		//email;
+		baza->insert("klienci", klient);
+	}
+	iloscDanych = baza->select("klienci", klienci);
+	idKlienta = szukajDanychWPolu(clientPesel.toStdString(), klienci, iloscDanych,3);
+	if (idKlienta != -1)		//jesli udalo sie stworzyc
+	{
+		iloscDanych = baza->select("pokoje", pokoje);
+		idPokoju = szukajDanychWPolu(clientRoomNumber.toStdString(), pokoje, iloscDanych, 2);
+		if(idPokoju != -1)
+		{
+			rezerwacja[1] = idPokoju;
+			rezerwacja[2] = idKlienta;
+			rezerwacja[3] = clientDate.toStdString();
+			rezerwacja[4] = clientDays.toStdString();
+			rezerwacja[5] = clientPeople.toStdString();		//tabela rezerwacji mila miec inne pola
+			baza->insert("rezerwacje", rezerwacja);
+			ui->listWidgetClients->addItem(
+				clientName + " " + clientSurname + " " + clientPesel + " " +
+				clientDate + " " + clientDays + " " + clientRoomNumber + " " + clientPeople
+			);
+		}
+		else
+		{
+			//nie ma pokoju
+		}
+	}
+	else
+	{
+		//error nie udalo sie stowrzyc klienta
+	}
 }
 
 void AdminWindow::on_pushButtonClientSearch_clicked()
--- HotelOrganizer2/adminwindow.h
+++ HotelOrganizer2/adminwindow.h
@@ -19,6 +19,7 @@ public:
     ~AdminWindow();
 
 private slots:
+	
     void on_pushButtonManageRooms_clicked();  // przycisk zarzadzania pokojami
 	void on_pushButtonClientAdd_clicked();    // przycisk dodawanie klienta
 	void on_pushButtonClientSearch_clicked(); // przycisk szukanie klienta
@@ -26,6 +27,8 @@ private slots:
 	void on_pushButtonClientDelete_clicked(); // przycisk usuwanie klienta
 
 private:
+	void konwertujStringNaDate(string StrData, struct tm *timeinfo);
+	int szukajDanychWPolu(string dana, string** tabela, int iloscDanych,int nrPola);	//zwraca id z tabeli a przyjmuje tablice z bazy i nr pola z ktorym ma porownywac
     Ui::AdminWindow *ui;
     RoomWindow *roomWindow; // przechowuje okno zarzadzania pokojami
 };
